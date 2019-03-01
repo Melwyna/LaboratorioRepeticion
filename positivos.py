314b1b7ec2a4800e1688597d023b0a1c79f44103
Lara Moreno <000403982@bga.upb>
2019-03-01 14:29:39 -0500

    Ejercicio dos resuelto

--- positivos.py
+++ positivos.py
@@ -1,5 +1,9 @@
-a=int(input("Ingrese un numero:"))
-if (a%2==0):
- print("El numero es par")
-if (a%2!=0):
- print("El numero es impar")
+contpa=0
+for a in range (1, 1000000000000):
+ a=int(input("Ingrese un numero:"))
+ if (a%2==0):
+  contpa+=1
+ else:
+  if (a%2!=0):
+    break
+print ("hubieron", contpa, "pares")
