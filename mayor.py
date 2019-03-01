15e4c22d93e0779f32977c248dbf313093a6c753
Lara Moreno <000403982@bga.upb>
2019-03-01 13:59:54 -0500

    Ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,5 +1,12 @@
-a=int(input("Ingrese un numero:"))
-if (a>=1):
- print("El numero es mayor que cero")
-else:
- print("El numero no es mayor que cero")
+contmay=0
+contmen=0
+contcer=0
+for n in range (1, 11):
+ n=int(input("ingrese los numeros"))
+ if (n>0):
+  contmay+=1
+ if (n<0):
+  contmen+=1
+ if (n==0):
+  contcer+=1
+print ("hubieron", contmay, "numeros mayores que cero", "y hubieron", contmen, "numeros menores que cero", "y", contcer, "numeros iguales a cero")
