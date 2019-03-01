1e98f029e77b97bdb853827eb6d25a3839d5b18f
Lara Moreno <000403982@bga.upb>
2019-03-01 14:58:10 -0500

    Ejercicio parte 4 resuelto

---
+++ promedio.py
@@ -0,0 +1,6 @@
+for a in range (1,2):
+ a=int(input("Ingrese un numero:"))
+ b=int(input("Ingrese un numero:"))
+ x1=(a+b)
+ x2=(x1/2)
+print ("la suma de los numeros es:", x1, "y el promedio de los numeros es:", x2)
